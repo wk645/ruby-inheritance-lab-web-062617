b8a5f5dcf572d8f61e56bf1e0c5005e046ca13a0
wk645 <wk645@nyu.edu>
2017-07-02 11:21:43 -0400

    lab

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+class Student < User
+
+	attr_accessor :knowledge
+	# innitializes with an empty knowledge array
+
+	def initialize
+		@knowledge = []
+	end
+
+	def learn(str)
+		@knowledge << str
+	end
+
+	def knowledge
+		@knowledge
+	end
 
 end
